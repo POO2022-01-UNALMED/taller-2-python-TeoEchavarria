b7d614c9ee6b725be34fc54fa7f149d3f860d186
Mateo Echavarria Sierra <echavarria.0042@gmail.com>
2022-03-20 17:36:45 -0500

    Creacion de Metodos para Auto
    Creo los metodos cantidadAsientos y verificarIntegridad.
    Cambio el tipo de self.asientos y self.motor

--- main.py
+++ main.py
@@ -7,26 +7,36 @@ class Asiento:
         self.precio = precio
         self.registro = registro
     
-    @staticmethod
     def cambiarColor(self, color):
         coloresPermitidos = ['rojo', 'verde', 'amarillo', 'negro', 'blanco']
         if color in coloresPermitidos:
             self.color = color
-
-class Auto:
-    def __init__(self, modelo, precio, asientos, marca, motor, registro):
-        self.modelo = modelo
-        self.precio = precio
-        self.asientos = asientos
-        self.marca = marca
-        self.motor = motor
-        self.registro = registro
 
 class Motor:
     def __init__(self, numerosCilindros, tipo, registro):
         self.numerosCilindros = numerosCilindros
         self.tipo = tipo
         self.registro = registro
+
+class Auto:
+    cantidadCreados = 0
+    def __init__(self, modelo, precio, asientos, marca, motor, registro):
+        self.modelo = modelo
+        self.precio = precio
+        self.asientos = list[Asiento]
+        self.marca = marca
+        self.motor = Motor
+        self.registro = registro
+
+    def cantidadAsientos(self):
+        return len(self.asientos)
+    
+    def verificarIntegridad(self):
+        if self.registro == self.motor.registro and sum([0 for i in self.asientos if i.registro == self.registro] == 0):
+            return 'Auto original'
+        else:
+            return 'Las piezas no son originales'
+    
 
 if __name__ == '__main__':
     pass
