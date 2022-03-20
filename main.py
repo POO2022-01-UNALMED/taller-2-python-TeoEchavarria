150f2bf068601eed4266de2bc745da8acc39565a
Mateo Echavarria Sierra <echavarria.0042@gmail.com>
2022-03-20 18:03:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
-from turtle import color
-
-
 class Asiento:
     def __init__(self, color, precio, registro):
         self.color = color
         self.precio = precio
         self.registro = registro
-    
+
     def cambiarColor(self, color):
         coloresPermitidos = ['rojo', 'verde', 'amarillo', 'negro', 'blanco']
         if color in coloresPermitidos:
             self.color = color
-
+  
 class Auto:
     cantidadCreados = 0
     def __init__(self, modelo, precio, asientos, marca, motor, registro):
@@ -21,18 +18,19 @@ class Auto:
         self.marca = marca
         self.motor = motor
         self.registro = registro
-
+        
     def cantidadAsientos(self):
-        return sum(1 for i in self.asientos if type(i) == Asiento else 0)
-    
+        return sum(1 for i in self.asientos if type(i) == Asiento)
+
     def verificarIntegridad(self):
-        for i in self.asientos:
-            if i != None:
-                if self.registro == i.registro and self.registro == self.motor.registro:
-                    return "Auto original"
-                else:
+        if self.registro == self.motor.registro:
+            for i in self.asientos:
+                if self.registro != i.registro:
                     return "Las piezas no son originales"
-                    
+            return "Auto original"
+        else:
+            return "Las piezas no son originales"
+
 class Motor:
     def __init__(self, numerosCilindros, tipo, registro):
         self.numerosCilindros = numerosCilindros
@@ -41,9 +39,8 @@ class Motor:
 
     def cambiarRegistro(self, registro):
         self.registro = registro
-    
+
     def asignarTipo(self, tipo):
         tiposPermitidos = ['electrico', 'gasolina']
         if tipo in tiposPermitidos:
             self.tipo = tipo
-    
